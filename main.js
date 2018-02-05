d2813a467ff1770ab193a30f0cd9ead9d6a0aa37
Zack Stickles <zstickles@gsandf.com>
2018-02-05 15:54:19 -0600

    Added authentication

--- main.js
+++ main.js
@@ -10,19 +10,28 @@ const config = {
 firebase.initializeApp(config);
 
 const db = firebase.database().ref();
+const auth = firebase.auth();
+const authProvider = new firebase.auth.GoogleAuthProvider();
+
+let username = null;
 
 // Get Elements
-const $username = document.getElementById('username');
 const $message = document.getElementById('message');
 const $postBtn = document.getElementById('post');
+const $loginBtn = document.getElementById('login');
 const $chat = document.getElementById('chat');
 
-// Post UI Event
+// When a user clicks on the post button
 $postBtn.addEventListener('click', () => {
 	const username = $username.value;
 	const message = $message.value;
 	db.push({ username, message });
 	$message.value = '';
+});
+
+// When a user clicks on the login button
+$loginBtn.addEventListener('click', () => {
+	auth.signInWithPopup(authProvider); // promise
 });
 
 // When a new message is recieved, add it
@@ -36,4 +45,15 @@ db.on('child_added', (snapshot) => {
 	$entry.appendChild($name);
 	$entry.appendChild($text);
 	$chat.appendChild($entry);
+});
+
+// When the user authentication status changes
+auth.onAuthStateChanged((data) => {
+	if (data) {
+		username = data.displayName;
+		$loginBtn.textContent = `Logged in as ${username}`;
+		$loginBtn.disabled = true;
+		$message.style.display = block;
+		$postBtn.style.display = block;
+	}
 });
